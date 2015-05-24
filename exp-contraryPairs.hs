5f82d4ff51f03e1185fde2f8dbfcc34bec97447b
Pavel Bocharov <pavel@wheely.com>
2015-05-24 14:45:43 +0300

    Refactored contraryPairs experiment.

--- exp-contraryPairs.hs
+++ exp-contraryPairs.hs
@@ -1,14 +1,14 @@
 import Lotto
+import Math.Combinat.Partitions
 import qualified Data.Set as S
 import Data.List (tails)
 
+contraryPairsSet :: [Partition] -> S.Set (Partition, Partition)
+contraryPairsSet xs = S.filter isContrary (uniquePairs (S.fromList xs))
+  where uniquePairs s = S.fromDistinctAscList [(x,y) | (x:xt) <- tails (S.toList s), y <- xt]
+        isContrary (x, y) = let (l, _, w) = tommyLotto x y in  signum (force x y) /= signum (w - l)
+
 main :: IO ()
-main = print contraryPairsSet
-  where
-    contraryPairsSet = S.filter isContrary (uniquePairs (S.fromList allPartitions))
-    uniquePairs s = S.fromDistinctAscList [(x,y) | (x:xt) <- tails (S.toList s), y <- xt]
-    allPartitions = allLottoPartitions n m
-    (n, m) = (8, 4)
-    isContrary (xs, ys) =
-      let (l, _, w) = tommyLotto xs ys
-      in  signum (force xs ys) /= signum (w - l)
+main = print $ contraryPairsSet xs
+  where xs = allLottoPartitions n m
+        (n, m) = (8, 4)
