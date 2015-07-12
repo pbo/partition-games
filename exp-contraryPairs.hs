3b6fe34dd447fb1df92a67acbd3c7a3d16d694e0
Pavel Bocharov <pavel@wheely.com>
2015-07-12 16:30:42 +0300

    Extracted uniquePairs function

--- exp-contraryPairs.hs
+++ exp-contraryPairs.hs
@@ -3,10 +3,12 @@ import Math.Combinat.Partitions
 import qualified Data.Set as S
 import Data.List (tails)
 
+uniquePairs :: [t] -> S.Set (t, t)
+uniquePairs xs = S.fromDistinctAscList [(x,y) | (x:xt) <- tails xs, y <- xt]
+
 contraryPairsSet :: [Partition] -> S.Set (Partition, Partition)
-contraryPairsSet xs = S.filter isContrary (uniquePairs (S.fromList xs))
-  where uniquePairs s = S.fromDistinctAscList [(x,y) | (x:xt) <- tails (S.toList s), y <- xt]
-        isContrary (x, y) = let (l, _, w) = tommyLotto x y in  signum (force x y) /= signum (w - l)
+contraryPairsSet xs = S.filter isContrary (uniquePairs xs)
+  where isContrary (x, y) = let (l, _, w) = tommyLotto x y in  signum (force x y) /= signum (w - l)
 
 main :: IO ()
 main = print $ contraryPairsSet xs
